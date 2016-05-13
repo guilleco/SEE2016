be46fa09949de40d6f164c6923580b49573f98c6
guilleco <guilleco@gmail.com>
2016-05-13 13:28:14 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-var nAplicacion ="appBase";
+var nAplicacion ="SEE2016";
 var urlMapea = "http://mapea-sigc.juntadeandalucia.es";
 var contextos = "wmcfile=http://www.callejerodeandalucia.es/wmc/context_cdau_callejero.xml*Callejero,http://www.callejerodeandalucia.es/wmc/context_cdau_satelite.xml*Satélite";
 var layers = "";
